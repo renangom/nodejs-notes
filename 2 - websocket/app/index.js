5bbd1ae9554ac4a76caafbf9a03763f1695d2678
Renan <renan23eduardo@gmail.com>
2022-12-11 11:15:50 -0300

    Create middleware check-auth and index.js routes

---
+++ 2 - websocket/app/index.js
@@ -0,0 +1,33 @@
+import e, { Router } from "express";
+import checkAuth from './check-auth'
+
+const router = Router();
+
+//rotas
+router.get('/', (req,res) => {
+    res.render('login')
+})
+
+router.post('/login', (req,res) => {
+    const user = req.body.username;
+    const password = req.body.password;
+
+    if(user === 'u1' && password === 'test'){
+        req.session.user = 'u1';
+    }else if(user === 'u2' && password === 'test'){
+        req.session.user = 'u2';
+    }
+
+    res.redirect('/chat')
+})
+
+router.get('/chat', checkAuth ,(req,res) => {
+    res.render('chat', {user: req.session.user})
+})
+
+router.get('/logout', (req,res) => {
+    delete req.session.user;
+    res.redirect('/')
+})
+
+export {router}
--- 2 - websocket/index.js
+++ 2 - websocket/index.js
@@ -3,8 +3,12 @@ import path from 'path'
 import url from 'url'
 import express from 'express'
 import cookieSession from 'cookie-session';
+import { router } from './app';
+
 
 const app = express();
+
+
 app.use(cookieSession({
     name: 'session',
     keys: ['key1', 'key2'],
@@ -15,10 +19,15 @@ app.use(express.urlencoded({extended: false}))
 app.set('views', `${path.dirname(url.fileURLToPath(import.meta.url))}/app/views`)
 app.set('view engine', 'pug')
 
+
+// ================== ROTAS ===================
+
 app.get('/', (req, res) => {
     res.render('login')
 })
 
+app.use(router)
+// ================ APP
 app.listen(8080, () => {
     console.log('Server is listenning to port 8080')
 })
